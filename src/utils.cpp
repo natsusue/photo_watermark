c483bf68ec3d6aeead8189b4a8442cf4075f3a08
natsusue <bugmaster@natsusue.tech>
2024-08-30 18:58:09 +0800

    修改linux错误

--- src/utils.cpp
+++ src/utils.cpp
@@ -6,6 +6,7 @@
 #define PATH_MAX MAX_PATH
 #endif
 #elif defined(__linux__) || defined(_LINUX)
+#include<limits.h>
 #include<unistd.h>
 #elif defined(__APPLE__)
 #include <mach-o/dyld.h>
@@ -18,12 +19,10 @@ std::filesystem::path GetSelfPath()
     GetModuleFileNameA(nullptr, path, PATH_MAX);
     return path;
 #elif defined(__linux__) || defined(_LINUX)
-    char path[PATH_MAX] = { 0 };
     ssize_t count = readlink("/proc/self/exe", path, PATH_MAX);
     return std::string(path, (count > 0)?count:0);
 #elif defined(__APPLE__)
     // 未测试,没mac设备
-    char path[PATH_MAX] = { 0 };
     uint32_t bufsize = PATH_MAX;
     if (!_NSGetExecutablePath(path, &bufsize))
         puts(buf);
